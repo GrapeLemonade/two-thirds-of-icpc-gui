4cce353f4022f6fa3f3c27b5edbd9e0c863f2990
Pantw <pantw@buaa.edu.cn>
2022-04-03 02:57:04 +0800

    添加 Vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: [
+    'vuetify'
+  ],
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
