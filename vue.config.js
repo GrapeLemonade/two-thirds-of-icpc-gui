2567bcbadff8b8f6e4c0ff65535d7635460beec6
Pantw <pantw@buaa.edu.cn>
2022-04-03 02:35:39 +0800

    指定输出 portable 应用

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        "win": {
+          "target": [
+              "portable"
+          ]
+        }
+      }
+    }
+  }
 })
